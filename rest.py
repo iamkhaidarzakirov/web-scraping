e804ba4981d645aef3064a6b99545014b3c2bf7b
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2022-12-18 17:58:38 +0300

    Add proxie

--- rest.py
+++ rest.py
@@ -1,18 +1,24 @@
 import requests
 import time
+import random
 
 
-def requests_debugger(url, headers, proxies=None, retry=5):
+def requests_debugger(url, headers, proxies=None, retry=2):
     try:
-        response = requests.get(url=url, headers=headers)
-        print(f'[+] {url} {response.status_code}')  # If the attempt was fortunate, print message
-    except Exception as ex:
-        ex = str(ex).split(':')[0]  # The exception's main short information
-        time.sleep(30)  # Delay between attempts to send a retry request; 30 seconds should be enough
-        if retry != 0:
-            print(f'{ex} | [INFO]: retry={retry} => {url}')
-            return requests_debugger(url, headers, retry=(retry - 1))  # Recursion / Next retry
-        else:
-            raise
-    else:
+        response = requests.get(url=url, headers=headers, proxies=None, timeout=10)  # Change None if needed connect
+        # proxy
+        print(f'[SUCCESS] {url} {response.status_code}')  # If the attempt was fortunate, print message
         return response
+    except Exception as ex:
+        if retry > 1:
+            time.sleep(20)  # Delay between attempts to send a retry request; 30 seconds should be enough
+            print(f'[ERROR]: retries left: {retry} => {url}')
+            return requests_debugger(url, headers, proxies=proxies, retry=(retry - 1))  # Recursion / Next retry
+        elif retry == 1:
+            print(f'[ERROR]: retries left: {retry} => {url}')
+            try:
+                response = requests.get(url=url, headers=headers, proxies=proxies, timeout=10)
+                print(f'[INFO] {proxies} ===> SUCCESS')
+                return response
+            except Exception:
+                raise
