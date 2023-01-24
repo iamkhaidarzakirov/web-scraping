aabe29dc8c7a7cc0a26104f1bfa97b83c7ea9031
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-01-24 11:27:57 +0300

    Delete rest.py

--- test.py
+++ test.py
@@ -1,28 +1,18 @@
-import datetime
-import time
+from rest import requests_debugger
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import fake_useragent
-
+import json
 # test your code here!
 
 
 def test():
-    curr_date = datetime.datetime.now()
-    for i in range(10):
-        print(curr_date, '# ' * 10, sep='\n')
-        time.sleep(10)
-        curr_date = datetime.datetime.now()
 
-
-def main():
-    test()
+    with open('myProxies.json', 'r', encoding='utf-8') as file:
+        proxies = json.load(file)
+        print(proxies)
 
 
 if __name__ == '__main__':
-    main()
+    test()
 
 
