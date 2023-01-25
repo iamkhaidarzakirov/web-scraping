fc5c62993a8e98e875ae55dc431ae71182351361
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-01-25 18:10:23 +0300

    [INFO] => INITIAL COMMIT

---
+++ requirements.py
@@ -0,0 +1,32 @@
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'location_data=C%3A12%3A%22LocationData%22%3A55%3A%7Ba%3A2%3A%7Bs%3A10%3A%22locationId%22%3Bs%3A4%3A%222705%22%3Bs%3A9%3A%22confirmed%22%3Bb%3A0%3B%7D%7D; PHPSESSID3=25v1jehu5se390k3spstg26in1; out_location_data=C%3A15%3A%22OutLocationData%22%3A48%3A%7Ba%3A2%3A%7Bs%3A10%3A%22locationId%22%3Bb%3A0%3Bs%3A9%3A%22confirmed%22%3Bb%3A0%3B%7D%7D; split=split-a; amp_3a3558=ouYEtxk3zyz5jelrfbdQyt...1gnhhk9af.1gnhhk9af.0.0.0; lastHost=www.avtoall.ru; USER_SALE_ID3=4898847078; _ga=GA1.2.1847288539.1674552420; _gid=GA1.2.390976609.1674552420; _gat=1; _ym_uid=16745524201032213872; _ym_d=1674552420; _ym_isad=2',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    # Requests doesn't support trailers
+    # 'TE': 'trailers',
+}
+
+cookies = {
+    'location_data': 'C%3A12%3A%22LocationData%22%3A55%3A%7Ba%3A2%3A%7Bs%3A10%3A%22locationId%22%3Bs%3A4%3A%222705%22%3Bs%3A9%3A%22confirmed%22%3Bb%3A0%3B%7D%7D',
+    'PHPSESSID3': '25v1jehu5se390k3spstg26in1',
+    'out_location_data': 'C%3A15%3A%22OutLocationData%22%3A48%3A%7Ba%3A2%3A%7Bs%3A10%3A%22locationId%22%3Bb%3A0%3Bs%3A9%3A%22confirmed%22%3Bb%3A0%3B%7D%7D',
+    'split': 'split-a',
+    'amp_3a3558': 'ouYEtxk3zyz5jelrfbdQyt...1gnhhk9af.1gnhhk9af.0.0.0',
+    'lastHost': 'www.avtoall.ru',
+    'USER_SALE_ID3': '4898847078',
+    '_ga': 'GA1.2.1847288539.1674552420',
+    '_gid': 'GA1.2.390976609.1674552420',
+    '_gat': '1',
+    '_ym_uid': '16745524201032213872',
+    '_ym_d': '1674552420',
+    '_ym_isad': '2',
+}
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-import json
 
 
 def test():
-
-    with open('myProxies.json', 'r', encoding='utf-8') as file:
-        proxies = json.load(file)
-        print(proxies)
+    ...
 
 
 if __name__ == '__main__':
