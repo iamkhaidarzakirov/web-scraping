7ab072a9a26e30300f0e8e0281b5f1e5f4667493
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-02-03 15:47:52 +0300

    Change proxies folder name

--- test_sync.py
+++ test_sync.py
@@ -9,13 +9,14 @@ from websiteData import headers
 def web_site_tester():
     # Check number or requests
     for i in range(1, 2):
+        url = ''
         if i % 10 == 0:
             time.sleep(1)
         proxy = random.choice(proxies_sync)
         response = requests.get(url=url, headers=headers, proxies=proxy)
         print(f'{response.status_code} WITH {proxy}')
         # Try to write an HTML file and look what will happen
-        with open(f'DATA/test_{i}.html', 'w', encoding='utf-8') as file:
+        with open(f'test/test_{i}.html', 'w', encoding='utf-8') as file:
             file.write(response.text)
         # Try to write an JSON file and look what will happen
         # with open(f'DATA/test_{i}.json', 'w', encoding='utf-8') as file:
@@ -27,6 +28,5 @@ if __name__ == '__main__':
     with open('DATA/proxies.json', 'r', encoding='utf-8') as json_file:
         proxies_sync = json.load(json_file)
     # Set parameters and let's go!
-    url = ''
     web_site_tester()
 
