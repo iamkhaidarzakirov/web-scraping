dfbb08783f426118511c972229f94c8d60a2606c
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-02-18 13:23:03 +0300

    * New simple project

---
+++ ontario-health-data/websiteData.py
@@ -0,0 +1,16 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    # 'Cookie': '_ga=GA1.2.838505607.1676703410; _gid=GA1.2.588572845.1676703410; _ga_5EMHF6S1M6=GS1.1.1676703421.1.1.1676703842.21.0.0; summary_id=63f076bc60a4e',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    # Requests doesn't support trailers
+    # 'TE': 'trailers',
+    'If-Modified-Since': 'Sat, 18 Feb 2023 07:04:00 GMT',
+}
