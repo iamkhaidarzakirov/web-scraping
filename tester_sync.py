a252724892e6b7789ceced7d02dbe961506fbc1e
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-02-18 13:33:22 +0300

    * Decline last test's url

--- tester_sync.py
+++ tester_sync.py
@@ -7,13 +7,15 @@ from websiteData import headers
 
 
 def web_site_tester():
-    url = 'https://www.abebooks.com/servlet/BookstoreSearch'
-    proxy = random.choice(proxies_sync)
-    response = requests.post(url=url, headers=headers)
-    print(f'{response.status_code} WITH {proxy}')
-    # Try to write an HTML file and look what will happen
-    with open(f'test/test.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    for i in range(1, 76):
+        url = f''
+        proxy = random.choice(proxies_sync)
+        response = requests.post(url=url, headers=headers)
+        print(f'{response.status_code} WITH {proxy}')
+        # Try to write an HTML file and look what will happen
+        if i == 1:
+            with open(f'test/test.html', 'w', encoding='utf-8') as file:
+                file.write(response.text)
 
 
 if __name__ == '__main__':
