f2df8a7ebe4ae5cdb51531637c61d2dea15ee7ea
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-03-16 16:00:41 +0300

    Added types annotations

--- pytesseract-files/utils.py
+++ pytesseract-files/utils.py
@@ -1,13 +1,12 @@
 import pytesseract
 from PIL import Image
-import requests
 
 
-def recognize(image_path):
+def recognize(image_path: str) -> str:
     # Tesseract engine
     pytesseract.pytesseract.tesseract_cmd = r'c:\tesseract\tesseract.exe'
     # Add image
     image = Image.open(image_path)
     text = pytesseract.image_to_string(image=image, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-    print(text)
+    return text
 
