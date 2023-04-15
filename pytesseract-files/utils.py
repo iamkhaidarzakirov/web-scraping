dc7bcf5562d87920a81575c5cdedaa9d6be9115d
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-04-15 13:04:40 +0300

    Add docstring in function

--- pytesseract-files/utils.py
+++ pytesseract-files/utils.py
@@ -3,10 +3,13 @@ from PIL import Image
 
 
 def recognize(image_path: str) -> str:
-    # Tesseract engine
+    """Function recognizes (if it is possible) text in image"""
+
+    # Tesseract engine path
     pytesseract.pytesseract.tesseract_cmd = r'c:\tesseract\tesseract.exe'
     # Add image
     image = Image.open(image_path)
     text = pytesseract.image_to_string(image=image, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
+
     return text
 
