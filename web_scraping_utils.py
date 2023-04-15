c60d6b5003bd781229c0545b6f644168365b03f9
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2023-04-15 12:58:20 +0300

    Add docstrings to functions

--- web_scraping_utils.py
+++ web_scraping_utils.py
@@ -1,10 +1,16 @@
+"""Module contains the functions which may using in routine web scraping projects"""
 import pandas as pd
 import os
 import json
+import re
 
 
-# 1. This function adds a row into a spreadsheet without rewriting a file
 def add_a_row_to_xlsx(path: str, df: pd.DataFrame, sheet_title: str = 'Sheet1') -> None:
+    """Using this function you can add one / or many (depending on DataFrame) strings to .xlsx spreadsheet.
+
+    Also, it allows to create table with many sheets and add a row to specific sheet
+    depending on which sheet name had specified."""
+
     if not os.path.exists(path):
         df.to_excel(path, sheet_name=sheet_title, index=False)
     else:
@@ -21,9 +27,14 @@ def add_a_row_to_xlsx(path: str, df: pd.DataFrame, sheet_title: str = 'Sheet1') 
                     df.to_excel(writer, startrow=0, sheet_name=sheet_title, index=False)
 
 
-# 2. This function creates json files with proxies in different formats
 def create_proxies_json(proxies_format: str, source_path: str, result_path: str) -> None:
-    # Open the file with Proxy6.net proxies list
+    """Function allows to create a json file with proxies depending on which proxies format had specified.
+
+    — Currently, the function supports to reformat proxies to aiohttp and requests modules formats.
+    — This function is configured to operate with proxies in the format provided by the site proxy6.net:
+        IP:PORT:LOGIN:PASSWORD
+    — The source file must be in .txt format; each proxy on a separate line"""
+
     with open(source_path) as file:
         raw_data = file.readlines()
         wrong_format = [item.replace('\n', '') for item in raw_data]
@@ -48,6 +59,41 @@ def create_proxies_json(proxies_format: str, source_path: str, result_path: str)
             print('[ERROR] Unknown format. Enter "aiohttp" or "requests", please.')
             break
         print(f'[INFO] {item} -> {value}')
+
+
+def del_special_symbols(some_string: str) -> str:
+    """Function deletes all special symbols in string using re module"""
+
+    edited_string = re.sub(r'[^\w\s]+|\d+', r'', string=some_string).strip()
+    return edited_string
+
+
+def count_unique_sites(data: list) -> dict:
+    """Function counts unique sites by domain in websites list"""
+
+    counter = {}
+    for item in data:
+        domain = item.split('/')[2]
+        counter[domain] = counter.get(domain, 0) + 1
+
+    return counter
+
+
+def sort_unique_sites(data: list) -> None:
+    """Function collects websites with the same domain to list and writes to json"""
+
+    sorter = {}
+    for item in data:
+        domain = item.split('/')[2]
+        sorter[domain] = []
+        for site in data:
+            if domain in site:
+                sorter[domain].append(site)
+
+    with open('unique_sites.json', 'w', encoding='utf-8') as file:
+        json.dump(sorter, file, indent=4, ensure_ascii=False)
+
+
 
 
 
